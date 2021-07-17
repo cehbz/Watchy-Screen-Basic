4eaaad0e254605f583174be04c4d33a5e1edd1f8
Charles Haynes <ceh+github@ceh.bz>
2021-07-17 19:26:39 +1000

    clean up includes, don't use using

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
-#include "Screen.h"
 #include "TimeScreen.h"
-#include "Watchy.h"  //include the Watchy library
-
-using namespace Watchy;
+#include "Watchy.h"
 
 void setup() {
   Serial.begin(921600);
   DEBUG("  %ld: setup %d\n", millis(), esp_sleep_get_wakeup_cause());
-  defaultScreen = &timeScreen;
 
-  Watchy::init();  // call init in setup
+  Watchy::defaultScreen = &timeScreen;
+  Watchy::init();
 }
 
-void loop() {
-  // this should never run, Watchy deep sleeps after init();
-}
+void loop() {}  // this should never run, Watchy deep sleeps after init();

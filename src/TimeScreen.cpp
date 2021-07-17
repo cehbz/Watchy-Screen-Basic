dca4b2fef6bdfae5470ccd2cb5978c40d2018e86
Charles Haynes <ceh+github@ceh.bz>
2021-07-17 19:26:59 +1000

    don't use using

--- src/TimeScreen.cpp
+++ src/TimeScreen.cpp
@@ -3,16 +3,14 @@
 #include "MenuScreen.h"
 #include "Watchy.h"
 
-using namespace Watchy;
-
 TimeScreen timeScreen;
 
 void TimeScreen::show() {
   tmElements_t currentTime;
-  RTC.read(currentTime);
-  display.setFont(&DSEG7_Classic_Bold_53);
-  display.setCursor(5, 53 + 60);
-  display.printf("%02d:%02d", currentTime.Hour, currentTime.Minute);
+  Watchy::RTC.read(currentTime);
+  Watchy::display.setFont(&DSEG7_Classic_Bold_53);
+  Watchy::display.setCursor(5, 53 + 60);
+  Watchy::display.printf("%02d:%02d", currentTime.Hour, currentTime.Minute);
 }
 
-void TimeScreen::menu() { setScreen(&menuScreen); }
+void TimeScreen::menu() { Watchy::setScreen(&menuScreen); }
